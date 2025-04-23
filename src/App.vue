7f407b67a4cbfcfa45838d6d0a271a1f3a2ef09b
Jordan Mullock <jordan@zenbit.uk>
2025-04-23 15:16:43 +0100

    restart as static app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  // const parent = "localhost"
+  const parent = "https://stream.boik.app"
+  const chat = `https://www.twitch.tv/embed/hatfilms/chat?parent=${parent}&darkpopout`;
+</script>
+
+<template>
+  <!-- <iframe
+      src="https://player.twitch.tv/?hatfilms&parent=localhost"
+      height="100%"
+      width="100%"
+      allowfullscreen="true"
+    >
+    </iframe> -->
+  <div className="vid">
+    <iframe
+      src="https://www.youtube.com/embed/live_stream?channel=UC7A_dLnSAjl7uROCdoNyjzg"
+      width="100%"
+      height="100%"
+      title="stream"
+    ></iframe>
+  </div>
+  <div className="chat">
+    <iframe
+      :src="chat"
+      height="100%"
+      width="100%"
+      title="chat"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped></style>
